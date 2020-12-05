0a2b6258d458c929d4de0ab24deceaf0286cf85b
zuchka <zuchka@protonmail.ch>
2020-12-04 23:03:46 -1000

    added flow control to rejects hyphens at index -2

--- lib/hyphen8/engine.ex
+++ lib/hyphen8/engine.ex
@@ -26,14 +26,14 @@ defmodule Hyphen8.Engine do
    def match_against_key(data) do
        data
        |> Keyword.put_new(:matches, Enum.map(data[:characters], fn word ->
-          Enum.map(Hyphen8.Table.main, fn [regex, _list]                 ->
+          Enum.map(Hyphen8.Table.main, fn [regex, _list]                ->
           Regex.scan(regex, word, return: :index) end)
           |> Enum.reject(&Enum.empty?/1 )
           |> Enum.map(   &List.flatten/1)
           |> Enum.map(   &Enum.map(&1, fn {a, _b} -> a end))
        end))
        |> Keyword.put_new(:regex, Enum.map(data[:characters], fn word   ->
-          Enum.flat_map(Hyphen8.Table.main, fn [regex, list]             ->
+          Enum.flat_map(Hyphen8.Table.main, fn [regex, list]            ->
             case Regex.match?(regex, word) do
                :true  -> [list]
                :false -> []
@@ -59,8 +59,8 @@ defmodule Hyphen8.Engine do
        Keyword.update!(data, :coordinates, fn list -> 
           Enum.map(list, &Enum.uniq(&1)
        |> Enum.map(     fn tuple    -> Tuple.to_list(tuple) end)
-       |> Enum.reject(  fn [  a,_b] -> a == 1               end)
-       |> Enum.group_by(fn [  a,_b] -> a                    end)
+       |> Enum.reject(  fn [  a,_b] -> a == 1               end) 
+       |> Enum.group_by(fn [  a,_b] -> a                    end) 
        |> Enum.map(     fn { _k, v} ->
           Enum.max_by(v,fn [ _a, b] -> b end)               end)
        |> Enum.reject(  fn [ _a, b] -> rem(b, 2) == 0       end)
@@ -76,7 +76,6 @@ defmodule Hyphen8.Engine do
        |> Enum.map(fn [word, coords] -> [String.split(word, "", trim: :true), Enum.map(coords, fn [a, _b] -> a end)]end)
        |> Enum.map(fn [word, coords] -> [List.to_tuple(word), coords]      end)
        |> Enum.map(fn [word, coords] -> Hyphen8.Recurse.main(word, coords) end)
-       |> Enum.map(&Tuple.to_list/1                      )
        |> Enum.map(&Enum.reject(&1, fn x -> x == "0" end))
        |> Enum.map(&Enum.join/1                          )
        |> Enum.join(" "                                  )
--- lib/hyphen8/recurse.ex
+++ lib/hyphen8/recurse.ex
@@ -1,17 +1,18 @@
 defmodule Hyphen8.Recurse do
 
+
     def main(data, []) do
-
-        data
-
+        data = Tuple.to_list(data)
+        unless Enum.at(data, -2) == "-" do
+            data
+        else
+            List.delete_at(data, -2)
+        end
     end
 
-
     def main(data, indices) do
-
         put_elem(data, hd(indices), "-")
         |> main(tl(indices))
-
     end
 
 end
