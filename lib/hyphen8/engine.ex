30a4ff4f8750e7aacd81027df4efcebb1536f478
zuchka <zuchka@protonmail.ch>
2020-12-09 11:51:39 -1000

    switched from enum to stream throughout

--- lib/hyphen8/engine.ex
+++ lib/hyphen8/engine.ex
@@ -12,76 +12,80 @@ defmodule Hyphen8.Engine do
 
    def parse_words(string) do
        Keyword.new
-       |> Keyword.put(:words, String.split(string, ~r/\s|\\n|\\r|\\t|\W|_/, trim: true))
+       |> Keyword.put_new(:words, String.split(string, ~r/\s|\\n|\\r|\\t|\W|_/, trim: true))
    end
 
    def parse_characters(data) do
-       data
-       |> Keyword.put( :characters, Enum.map(data[:words],
+       characters =
+       Stream.map(data[:words],
           &String.split(&1, ~r/\w/, include_captures: true, trim: true))
-       |> Enum.map(&Enum.join(&1, "0"))
-       |> Enum.map(fn word -> "0#{word}0" end)
-       )
+       |> Stream.map(&Enum.join(&1, "0"))
+       |> Stream.map(fn word -> "0#{word}0" end)
+
+       Keyword.put_new(data, :characters, Enum.to_list(characters))
    end
 
    def match_against_key(data) do
-       data
-       |> Keyword.put_new(:matches, Enum.map(data[:characters], fn word ->
-          Enum.map(Hyphen8.Table.main, fn [regex, _list]                ->
+       matches =
+       Stream.map(data[:characters], fn word ->
+          Stream.map(Hyphen8.Table.main, fn [regex, _list]              ->
           Regex.scan(regex, word, return: :index) end)
-          |> Enum.reject(&Enum.empty?/1 )
-          |> Enum.map(   &List.flatten/1)
-          |> Enum.map(   &Enum.map(&1, fn {a, _b} -> a end))
-       end))
-       |> Keyword.put_new(:regex, Enum.map(data[:characters], fn word   ->
+          |> Stream.reject(&Enum.empty?/1 )
+          |> Stream.map(   &List.flatten/1)
+          |> Enum.map(     &Enum.map(&1, fn {a, _b} -> a end))
+       end)
+
+       regex =
+       Stream.map(data[:characters], fn word   ->
           Enum.flat_map(Hyphen8.Table.main, fn [regex, list]            ->
             case Regex.match?(regex, word) do
                :true  -> [list]
                :false -> []
             end
           end)
-       end))
+       end)
+
+       Keyword.put_new(data, :matches, Enum.to_list(matches))
+       |> Keyword.put_new(:regex, Enum.to_list(regex))
    end
 
    def compile_coords(data) do
-       Keyword.put_new(data, :coordinates,
-          Enum.zip(data[:matches], data[:regex]                             )
-       |> Enum.map(&Tuple.to_list/1    )
-       |> Enum.map(fn [match, regex] -> Enum.zip(     match, regex          )
-       |> Enum.map(&Tuple.to_list/1    )
-       |> Enum.map(fn [indices, tuples] -> Enum.map(indices, fn index      ->
+       stream =
+          Stream.zip(data[:matches], data[:regex]                             )
+       |> Stream.map(&Tuple.to_list/1    )
+       |> Stream.map(fn [match, regex] -> Enum.zip(     match, regex          )
+       |> Stream.map(&Tuple.to_list/1    )
+       |> Stream.map(fn [indices, tuples] -> Enum.map(indices, fn index      ->
           Enum.map(tuples, fn {a, b} -> {(a + index), b}         end)end)end)
        |> Enum.flat_map(&List.flatten/1)
        end)
-       )
+
+       Keyword.put_new(data, :coordinates, Enum.to_list(stream))
    end
 
    def filter_coords(data) do
-       Keyword.update!(data, :coordinates, fn list -> 
-          Enum.map(list, &Enum.uniq(&1)
-       |> Enum.map(     fn tuple    -> Tuple.to_list(tuple) end)
-       |> Enum.reject(  fn [  a,_b] -> a == 0               end)
-       |> Enum.reject(  fn [  a,_b] -> a == 2               end)
-       |> Enum.group_by(fn [  a,_b] -> a                    end) 
-       |> Enum.map(     fn { _k, v} ->
-          Enum.max_by(v,fn [ _a, b] -> b end)               end)
-       |> Enum.reject(  fn [ _a, b] -> rem(b, 2) == 0       end)
+       stream =
+          Stream.map(data[:coordinates], &Enum.uniq(&1)
+       |> Stream.map(     fn tuple    -> Tuple.to_list(tuple) end)
+       |> Stream.reject(  fn [  a,_b] -> a == 0               end)
+       |> Stream.reject(  fn [  a,_b] -> a == 2               end)
+       |> Enum.group_by(  fn [  a,_b] -> a                    end)
+       |> Stream.map(     fn { _k, v} ->
+          Enum.max_by(v,  fn [ _a, b] -> b end)               end)
+       |> Enum.reject(    fn [ _a, b] -> rem(b, 2) == 0       end)
        )
-       end)
+
+       Keyword.put_new(data, :filtered, Enum.to_list(stream))
    end
 
    def construct_hyphens(data) do
-       data
-       |> Keyword.put_new(:hyphenated, 
-          Enum.zip(data[:characters], data[:coordinates])
-       |> Enum.map(&Tuple.to_list/1)
-       |> Enum.map(fn [word, coords] -> [String.split(word, "", trim: :true), Enum.map(coords, fn [a, _b] -> a end)]end)
-       |> Enum.map(fn [word, coords] -> [List.to_tuple(word), coords]      end)
-       |> Enum.map(fn [word, coords] -> Hyphen8.Recurse.main(word, coords) end)
-       |> Enum.map(&Enum.reject(&1, fn x -> x == "0" end))
-       |> Enum.map(&Enum.join/1                          )
-       |> Enum.join(" "                                  )
-       )
-       |> Keyword.get(:hyphenated)
+       Stream.zip(data[:characters], data[:filtered])
+       |> Stream.map(&Tuple.to_list/1)
+       |> Stream.map(fn [word, coords] -> [String.split(word, "", trim: :true), Enum.map(coords, fn [a, _b] -> a end)]end)
+       |> Stream.map(fn [word, coords] -> [List.to_tuple(word), coords]      end)
+       |> Stream.map(fn [word, coords] -> Hyphen8.Recurse.main(word, coords) end)
+       |> Stream.map(&Enum.reject(&1, fn x -> x == "0" end))
+       |> Stream.map(&Enum.join/1                          )
+       |> Enum.join(" "                                    )
    end
 end
--- lib/hyphen8/recurse.ex
+++ lib/hyphen8/recurse.ex
@@ -3,7 +3,7 @@ defmodule Hyphen8.Recurse do
 
     def main(data, []) do
         data = Tuple.to_list(data)
-        unless Enum.at(data, -3) == "-"
+        unless Enum.at(data, -3) == "-" do
             data
         else
             List.delete_at(data, -3)
